569e5b8d5c6f47191e3abb7e7f13a36483dcdd6d
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2017-05-29 10:05:11 +0900

    first commit

---
+++ token/token.go
@@ -0,0 +1,20 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT"
+	INT = "INT"
+
+	// Operators
+	ASSIGN = "ASSIGN"
+)
