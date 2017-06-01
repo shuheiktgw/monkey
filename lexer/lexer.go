1b3ae56d3c1b1e5d219b22f45f701dcda86f405c
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2017-06-01 09:33:03 +0900

    fix lexer test to pass

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "../token"
+
 type Lexer struct {
 	input string
 	position int
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -15,7 +15,7 @@ func TestNextToken(t *testing.T) {
 		{token.ASSIGN, "="},
 		{token.PLUS, "+"},
 		{token.LPAREN, "("},
-		{token.RPAREN, "("},
+		{token.RPAREN, ")"},
 		{token.LBRACE, "{"},
 		{token.RBRACE, "}"},
 		{token.COMMA, ","},
@@ -25,14 +25,14 @@ func TestNextToken(t *testing.T) {
 
 	l := New(input)
 
-	for i, tt = range tests {
+	for i, tt := range tests {
 		tok := l.NextToken()
 
 		if tok.Type != tt.expectedType {
 			t.Fatalf("tests[%d] - tokentype wrong. expected = %q, got = %q", i, tt.expectedType, tok.Type)
 		}
 
-		if tok.LITERAL != tt.expectedLiteral {
+		if tok.Literal != tt.expectedLiteral {
 			t.Fatalf("tests[%d] - literal wrong. expected = %q, got = %q", i, tt.expectedLiteral, tok.Literal)
 		}
 	}
