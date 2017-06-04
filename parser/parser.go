3e12413ddb91482f4ff17720978b4ff96d244a4f
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2017-06-05 08:36:15 +0900

    add peekError method

--- parser/parser.go
+++ parser/parser.go
@@ -4,21 +4,35 @@ import (
 	"../ast"
 	"../lexer"
 	"../token"
+	"fmt"
 )
 
 type Parser struct {
 	l *lexer.Lexer
 	curToken token.Token
 	peekToken token.Token
+	errors []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l: l,
+		errors: []string{},
+	}
 
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead.", t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken() {
@@ -83,6 +97,7 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 		p.nextToken()
 		return true
 	} else {
+		p.peekError(t)
 		return false
 	}
 }
