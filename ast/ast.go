cf3fdffb4bbdaef4af10f7084a3e09b1d59b7408
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2017-06-05 10:26:53 +0900

    add String method to each asts

--- ast/ast.go
+++ ast/ast.go
@@ -2,10 +2,12 @@ package ast
 
 import (
 	"../token"
+	"bytes"
 )
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -34,6 +36,16 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 // ======================
 // Let
 // ======================
@@ -47,6 +59,22 @@ type LetStatement struct {
 func (ls * LetStatement) statementNode() {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
+func (ls *LetStatement) String() {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() +  " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
 // ======================
 // Return
 // ======================
@@ -59,6 +87,40 @@ type ReturnStatement struct {
 func (rs *ReturnStatement) statementNode(){}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
 
+func (rs *ReturnStatement) String() {
+	var out bytes.Buffer
+
+	out.WriteString(rs.TokenLiteral() +  " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+
+// ======================
+// Expression
+// ======================
+
+type ExpressionStatement struct {
+	Token token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode(){}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (es *ExpressionStatement) String() {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+
+	return ""
+}
+
 // ======================
 // Identifier
 // ======================
@@ -70,3 +132,5 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode() {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+func (i *Identifier) String() { return i.Value }
