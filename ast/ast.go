dc0b609ff012046bfda1312d5d9f398ab12c93d2
Shuhei Kitagawa <shuhei.kitagawa@c-fo.com>
2017-06-05 10:44:29 +0900

    wrote a test for String method

--- ast/ast.go
+++ ast/ast.go
@@ -59,7 +59,7 @@ type LetStatement struct {
 func (ls * LetStatement) statementNode() {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
 
-func (ls *LetStatement) String() {
+func (ls *LetStatement) String() string {
 	var out bytes.Buffer
 
 	out.WriteString(ls.TokenLiteral() +  " ")
@@ -87,7 +87,7 @@ type ReturnStatement struct {
 func (rs *ReturnStatement) statementNode(){}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
 
-func (rs *ReturnStatement) String() {
+func (rs *ReturnStatement) String() string {
 	var out bytes.Buffer
 
 	out.WriteString(rs.TokenLiteral() +  " ")
@@ -113,7 +113,7 @@ type ExpressionStatement struct {
 func (es *ExpressionStatement) statementNode(){}
 func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
 
-func (es *ExpressionStatement) String() {
+func (es *ExpressionStatement) String() string {
 	if es.Expression != nil {
 		return es.Expression.String()
 	}
@@ -133,4 +133,4 @@ type Identifier struct {
 func (i *Identifier) expressionNode() {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
 
-func (i *Identifier) String() { return i.Value }
+func (i *Identifier) String() string { return i.Value }
